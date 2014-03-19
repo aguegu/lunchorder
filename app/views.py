e9a02d60c2f72a1896c0b37c3a10db626bcf7b8a
Weihong Guan <weihong.guan@gmail.com>
2014-03-19 18:34:13 +0800

    get restaurant

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
-from app import app
+# -*- coding: utf-8 -*-
+
+from app import app, models, db
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
 def index():
   return 'Lunch Order'
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+  print models.Restaurant.query.all()
+  return '%r' % models.Restaurant.query.all()[0].name
+
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+  r = models.Restaurant.query.filter_by(id=int(id))
+  return "%r" % r[0].name
+
