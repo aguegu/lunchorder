c7eaa7a160e638e87408871e1722dbab1d8bf7a0
Weihong Guan <weihong.guan@gmail.com>
2014-04-08 00:19:11 +0800

    init add restaurant dialog

--- run.py
+++ run.py
@@ -5,7 +5,7 @@
 from app import app, sock, views
 from threading import Thread
 
-Thread(target=views.background_thread).start()
+#Thread(target=views.background_thread).start()
 sock.run(app, host='0.0.0.0')
 
 
