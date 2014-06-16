39f575ade69894e1517b91f3de4257782de863e7
Weihong Guan <weihong.guan@gmail.com>
2014-06-16 21:21:48 +0800

    use flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.socketio import SocketIO
 
 app = Flask(__name__)
 app.config.from_object('config')
+Bootstrap(app)
 sock = SocketIO(app)
 db = SQLAlchemy(app)
 
